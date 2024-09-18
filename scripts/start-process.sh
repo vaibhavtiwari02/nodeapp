093563bbc0cbaec43e7eff966d6e0ace6822b784
Vaibhav Tiwari <91326468+vaibhavtiwari02@users.noreply.github.com>
2024-09-18 21:33:43 +0530

    Update and rename start.sh to start-process.sh

---
+++ scripts/start-process.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+container_name=myapp
+
+# ECR Login
+#aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 123456789012.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 207567793991.dkr.ecr.us-east-1.amazonaws.com
+
+
+# Pulling image from ECR
+docker pull 207567793991.dkr.ecr.us-east-1.amazonaws.com/mynodeapp:latest
+
+# Stop and remove the current container
+docker rm -f $container_name
+
+# Creating and starting a docker container using a new image
+docker run -d -p 3000:3000 --name $container_name 207567793991.dkr.ecr.us-east-1.amazonaws.com/mynodeapp:latest
--- scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Stop and remove any existing containers
-docker stop myapp || true
-docker rm myapp || true
-
-# Run the Docker container
-docker run -d -p 3000:3000 --name myapp myapp:latest
-
