29ee75cd1150d352ad4687d3954b2bc049aa788c
Vaibhav Tiwari <91326468+vaibhavtiwari02@users.noreply.github.com>
2024-09-18 21:31:14 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,6 @@ phases:
       - name: Install dependencies
         command: npm install
       - name: Build Docker image
-        command: docker push 207567793991.dkr.ecr.us-east-1.amazonaws.com/myapp:latest .
+        command: docker build -t 207567793991.dkr.ecr.us-east-1.amazonaws.com/mynodeapp:latest .
       - name: Push Docker image to ECR
         command: docker push 207567793991.dkr.ecr.us-east-1.amazonaws.com/myapp:latest
