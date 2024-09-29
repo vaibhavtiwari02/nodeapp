101e144db1a3ed24ab1aaa37b6a88ce1d1a0f67c
Vaibhav Tiwari <91326468+vaibhavtiwari02@users.noreply.github.com>
2024-09-29 20:49:08 +0530

    Update start-process.sh

--- scripts/start-process.sh
+++ scripts/start-process.sh
@@ -7,10 +7,10 @@ container_name=myapp
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 207567793991.dkr.ecr.us-east-1.amazonaws.com
 
 # Pulling image from ECR
-docker pull 207567793991.dkr.ecr.us-east-1.amazonaws.com/myapp:latest
+docker pull 207567793991.dkr.ecr.us-east-1.amazonaws.com/mynodeapp:latest
 
 # Stop and remove the current container
 docker rm -f $container_name
 
 # Creating and starting a docker container using a new image
-docker run -d -p 3000:3000 --name $container_name 207567793991.dkr.ecr.us-east-1.amazonaws.com/myapp:latest
+docker run -d -p 3000:3000 --name $container_name 207567793991.dkr.ecr.us-east-1.amazonaws.com/mynodeapp:latest
