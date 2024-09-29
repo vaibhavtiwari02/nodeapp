b09b51323e420855b59e568ec7f3cb03fb6a1c67
Vaibhav Tiwari <91326468+vaibhavtiwari02@users.noreply.github.com>
2024-09-29 21:05:20 +0530

    Create after_install.sh

---
+++ scripts/after_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Login to ECR
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 207567793991.dkr.ecr.us-east-1.amazonaws.com
+docker build -t myapp .
+docker tag myapp:latest 207567793991.dkr.ecr.us-east-1.amazonaws.com/myapp:latest
+docker push 207567793991.dkr.ecr.us-east-1.amazonaws.com/myapp:latest
